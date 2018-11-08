559de81794f59c5421a7a62dbaadaebbc283c4e5
Archana <archana.kumari@kelltontech.com>
2018-11-08 12:51:57 +0530

    first commit

---
+++ SplitSentence/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SplitSentence
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter Your String To Split");
+            string userInput = Console.ReadLine();
+            // Split string on spaces.
+            // ... This will separate all the words.
+            Console.WriteLine("\n");
+
+            Console.WriteLine("Splited Words Are:");
+
+            string[] words = userInput.Split(' ');
+            foreach (string word in words)
+            {
+                Console.WriteLine(word);
+                
+            }
+
+            Console.ReadLine();
+        }
+    }
+    
+}
